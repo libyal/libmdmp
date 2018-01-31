59101eb7f0fe538150bc90bd4fab0553b56302d4
Joachim Metz <joachim.metz@gmail.com>
2018-01-31 23:02:08 +0100

    Applied updates

--- mdmptools/mdmptools_output.c
+++ mdmptools/mdmptools_output.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the mdmptools
  *
- * Copyright (C) 2014-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2014-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -101,7 +101,7 @@ void mdmpoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2014-2017, %s.\n" ),
+	 _( "Copyright (C) 2014-2018, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
