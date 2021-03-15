2da92f3bd8f8cf5a4fcdd21d761f9723fdf76f5f
Joachim Metz <joachim.metz@gmail.com>
2021-03-15 08:03:57 +0100

    Worked on tests

--- tests/mdmp_test_tools_output.c
+++ tests/mdmp_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../mdmptools/mdmptools_output.h"
 
-/* Tests the mdmptools_output_initialize and function
+/* Tests the mdmptools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int mdmp_test_tools_output_initialize(
