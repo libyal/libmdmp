406014771db6451c7cc10bda81c4d22c374d41e4
Joachim Metz <joachim.metz@gmail.com>
2026-05-07 10:28:36 +0200

    Applied updates

---
+++ synctestdata.ps1
@@ -0,0 +1,25 @@
+# Script that synchronizes the local test data
+#
+# Version: 20251217
+
+$Repository = "log2timeline/plaso"
+$TestDataPath = "test_data"
+$TestSet = "public"
+$TestInputDirectory = "tests/input"
+$TestFiles = "WER1090.tmp.mdmp"
+
+If (-Not (Test-Path ${TestInputDirectory}))
+{
+	New-Item -Name ${TestInputDirectory} -ItemType "directory" | Out-Null
+}
+If (-Not (Test-Path "${TestInputDirectory}\${TestSet}"))
+{
+	New-Item -Name "${TestInputDirectory}\${TestSet}" -ItemType "directory" | Out-Null
+}
+ForEach ($TestFile in ${TestFiles} -split " ")
+{
+	$Url = "https://raw.githubusercontent.com/${Repository}/refs/heads/main/${TestDataPath}/${TestFile}"
+
+	Invoke-WebRequest -Uri ${Url} -OutFile "${TestInputDirectory}\${TestSet}\${TestFile}"
+}
+
